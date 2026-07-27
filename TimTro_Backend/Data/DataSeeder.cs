0cbfff89740c8a97a581c18a1b4a15acd49289c0
van-0201 <dauhuyvan23122021@gmail.com>
2026-07-27 17:13:36 +0700

    Remove dummy user accounts from DataSeeder for security

--- TimTro_Backend/Data/DataSeeder.cs
+++ TimTro_Backend/Data/DataSeeder.cs
@@ -41,66 +41,6 @@ namespace TimTro_Backend.Data
                 });
             }
 
-            if (!context.Users.Any(u => u.Email == "chutro@phongtro.vn"))
-            {
-                context.Users.Add(new User
-                {
-                    Id = Guid.NewGuid(),
-                    HoTen = "Nguyễn Chủ Trọ",
-                    Email = "chutro@phongtro.vn",
-                    SoDienThoai = "0333444555",
-                    MatKhauBam = BCrypt.Net.BCrypt.HashPassword("ChuTro@123"),
-                    VaiTro = "ChuTro",
-                    TrangThaiTaiKhoan = true,
-                    NgayTao = DateTime.Now
-                });
-            }
-
-            if (!context.Users.Any(u => u.Email == "nguoithue@phongtro.vn"))
-            {
-                context.Users.Add(new User
-                {
-                    Id = Guid.NewGuid(),
-                    HoTen = "Trần Người Thuê",
-                    Email = "nguoithue@phongtro.vn",
-                    SoDienThoai = "0666777888",
-                    MatKhauBam = BCrypt.Net.BCrypt.HashPassword("NguoiThue@123"),
-                    VaiTro = "NguoiThue",
-                    TrangThaiTaiKhoan = true,
-                    NgayTao = DateTime.Now
-                });
-            }
-
-            if (!context.Users.Any(u => u.Email == "thuetro1@phongtro.vn"))
-            {
-                context.Users.Add(new User
-                {
-                    Id = Guid.NewGuid(),
-                    HoTen = "Lê Thuê Trọ 1",
-                    Email = "thuetro1@phongtro.vn",
-                    SoDienThoai = "0111222333",
-                    MatKhauBam = BCrypt.Net.BCrypt.HashPassword("ThueTro1@123"),
-                    VaiTro = "NguoiThue",
-                    TrangThaiTaiKhoan = true,
-                    NgayTao = DateTime.Now
-                });
-            }
-
-            if (!context.Users.Any(u => u.Email == "thuetro2@phongtro.vn"))
-            {
-                context.Users.Add(new User
-                {
-                    Id = Guid.NewGuid(),
-                    HoTen = "Phạm Thuê Trọ 2",
-                    Email = "thuetro2@phongtro.vn",
-                    SoDienThoai = "0444555666",
-                    MatKhauBam = BCrypt.Net.BCrypt.HashPassword("ThueTro2@123"),
-                    VaiTro = "NguoiThue",
-                    TrangThaiTaiKhoan = true,
-                    NgayTao = DateTime.Now
-                });
-            }
-
             context.SaveChanges();
         }
     }
